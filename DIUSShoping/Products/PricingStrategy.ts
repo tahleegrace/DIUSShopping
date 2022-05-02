84c46db206ae5238d0b332f01fb3998d262b8dd8
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-02 15:10:43 +1000

    Added cart property to PricingStrategy class.

--- DIUSShoping/Products/PricingStrategy.ts
+++ DIUSShoping/Products/PricingStrategy.ts
@@ -1,7 +1,19 @@
 // A strategy for working out the pricing and quantity supplied of a product in a shopping cart.
 abstract class PricingStrategy {
+    // The items in the shopping cart.
+    _cart: Product[];
+
+    get cart() {
+        return this._cart;
+    }
+
+    set cart(value: Product[]) {
+        this._cart = value;
+    }
+
     abstract unitPrice(): number;
     abstract totalPrice(): number;
+    abstract quantity(): number;
     abstract accessories(): Product[];
     abstract getInstance(): PricingStrategy;
 }
