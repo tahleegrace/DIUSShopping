68f337fe5788c95b82b9f4a4efcb93e797c715e7
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-02 15:23:27 +1000

    Renamed PricingStrategy to PricingRule.

--- DIUSShoping/Pricing/PricingRule.ts
+++ DIUSShoping/Pricing/PricingRule.ts
@@ -1,5 +1,5 @@
 // A strategy for working out the pricing and quantity supplied of a product in a shopping cart.
-abstract class PricingStrategy {
+abstract class PricingRule {
     // The items in the shopping cart.
     _cart: Product[];
 
@@ -13,7 +13,7 @@ abstract class PricingStrategy {
 
     abstract unitPrice(): number;
     abstract totalPrice(): number;
-    abstract quantity(): number;
+    abstract quantity(): number; // How many products will be delivered.
     abstract accessories(): Product[];
-    abstract getInstance(): PricingStrategy;
+    abstract getInstance(): PricingRule;
 }
